dd0483b0d82c2830669584502d1a0fd395f1a889
miladaleali <miladaleali1392@gmail.com>
2023-05-31 09:17:35 +0000

    ADD: add engines to station

--- backend/app/app/station/settings.py
+++ backend/app/app/station/settings.py
@@ -2,7 +2,12 @@ from pedesis.conf.global_settings import BaseStationSettings
 
 
 class StationSettings(BaseStationSettings):
-    installed_engines: list[str] = []  # populate this with engine names.
+    installed_engines: list[str] = [
+        'multi_long_triple_ema_scalp',
+        'scalp_short_macd',
+        'swing_long_bollinger_bands',
+        'swing_short_bollinger_bands',
+    ]  # populate this with engine names.
 
     installed_brokers: list[str] = [
         'pedesis.components.broker.templates.okx',
