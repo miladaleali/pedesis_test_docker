265aac65a802d14454201979917e9deef3a61df6
miladaleali <miladaleali1392@gmail.com>
2023-06-15 16:21:54 +0000

    FIX: typo

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -72,7 +72,7 @@ def run_migrations_online() -> None:
 
     """
     configuration = config.get_section(config.config_ini_section)
-    configuration["sqlalchemy.url"] = get_url()
+    configuration["sqlalchemy.url"] = get_url() if os.environ['DB_SERVICE'] != 'neon' else get_neon_url()
     connectable = engine_from_config(
         configuration,
         prefix="sqlalchemy.",
