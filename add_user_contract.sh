1a8255285ddb107a48d92cb4ef4da161cea88281
miladaleali <miladaleali1392@gmail.com>
2023-06-19 07:58:08 +0000

    FIX: Err

--- add_user_contract.sh
+++ add_user_contract.sh
@@ -6,7 +6,7 @@ echo "Create NEW USER..."
 docker compose exec backend python -m pedesis create usertest milad aleali miladaleali miladaleali1392@gmail.com 09921374939 123456
 
 echo "Create NEW USER BROKER..."
-docker compose exec backend python -m pedesis create userborkertest miladaleali 123456 $DEBUG_BROKER_API_KEY $DEBUG_BROKER_SECRET_KEY $DEBUG_BROKER_PASSPHRASE
+docker compose exec backend python -m pedesis create userborkertest miladaleali 123456 okx $DEBUG_BROKER_API_KEY $DEBUG_BROKER_SECRET_KEY $DEBUG_BROKER_PASSPHRASE
 
 echo "Create NEW CONTRACT..."
 docker compose exec backend python -m pedesis create contracttest --add-to-station mildaleali okx 32000 50 360 123456
