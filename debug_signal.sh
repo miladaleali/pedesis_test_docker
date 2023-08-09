098231f5fa22107b25ffbdeb6ab6dfcdd7de8710
miladaleali <miladaleali1392@gmail.com>
2023-08-09 08:35:59 +0000

    ADD: position status cli report

--- debug_signal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Debug Send Signal"
-docker compose exec backend python ./debug/send_signal.py
--- rebuild_stack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Build stack without cache..."
-docker compose --profile logs build --no-cache
-
-echo "Stack up..."
-docker compose --profile logs up
