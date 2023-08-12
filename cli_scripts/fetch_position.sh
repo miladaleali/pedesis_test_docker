7de5e9c53e4eafb054c2feed27bb89600d87c5ea
miladaleali <miladaleali1392@gmail.com>
2023-08-12 12:49:42 +0000

    RENAME

---
+++ cli_scripts/fetch_position.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "What command do you want run?"
+read command_
+
+if [ "$command_" = "fetchone"]; then
+    # Ask which position
+    echo "Give Position id:"
+    read position_id
+
+    docker compose exec backend python ./debug/position_status.py fetchone $position_id
+
+else
+    docker compose exec backend python ./debug/position_status.py fetchall
+
+fi
+
+
---
+++ cli_scripts/safe_down.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+echo "Down Docker..."
+docker compose down --remove-orphans
+
+# echo "Remove Volumes..."
+# docker volume rm pedesis_redis-data
+
+# echo "Remove Celery worker id..."
+# sudo rm -r ../celery_files/
