a8c26486f7e4b34dce7e994f98d2d4895b67b8bc
miladaleali <miladaleali1392@gmail.com>
2023-08-27 04:23:55 +0000

    ADD: sub account for test

--- cli_scripts/add_user_contract.sh
+++ cli_scripts/add_user_contract.sh
@@ -6,7 +6,10 @@ echo "Create NEW USER..."
 docker compose exec backend python -m pedesis create usertest milad aleali miladaleali miladaleali1392@gmail.com 09921374939 123456
 
 echo "Create NEW USER BROKER..."
-docker compose exec backend python -m pedesis create userbrokertest miladaleali 123456 okx $DEBUG_BROKER_API_KEY $DEBUG_BROKER_SECRET_KEY $DEBUG_BROKER_PASSPHRASE
+docker compose exec backend python -m pedesis create userbrokertest miladaleali 123456 okx $DEBUG_BROKER_API_KEY $DEBUG_BROKER_SECRET_KEY $DEBUG_BROKER_PASSPHRASE master
+docker compose exec backend python -m pedesis create userbrokertest miladaleali 123456 okx $DEBUG_BROKER_API_KEY1 $DEBUG_BROKER_SECRET_KEY1 $DEBUG_BROKER_PASSPHRASE1 sub_account pedesistest1
+docker compose exec backend python -m pedesis create userbrokertest miladaleali 123456 okx $DEBUG_BROKER_API_KEY2 $DEBUG_BROKER_SECRET_KEY2 $DEBUG_BROKER_PASSPHRASE2 sub_account pedesistest2
+docker compose exec backend python -m pedesis create userbrokertest miladaleali 123456 okx $DEBUG_BROKER_API_KEY3 $DEBUG_BROKER_SECRET_KEY3 $DEBUG_BROKER_PASSPHRASE3 sub_account pedesistest3
 
 echo "Create NEW CONTRACT..."
-docker compose exec backend python -m pedesis create contracttest --add-to-station miladaleali okx 32000 50 360 123456
+docker compose exec backend python -m pedesis create contracttest --add-to-station miladaleali okx 52000 50 360 123456
